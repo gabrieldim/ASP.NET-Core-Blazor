5a03d65dbabd83538fc8c50c83523116ca99eb80
gabrieldim <dimitrievskig000@gmail.com>
2023-06-16 16:34:38 +0200

    Events, Callback functions, Render Fragment, styling

---
+++ BlazorServerApp/BlazorServerApp/Pages/Index.razor.cs
@@ -0,0 +1,42 @@
+﻿using BlazorServerApp.Models;
+
+namespace BlazorServerApp.Pages
+{
+    public partial class Index
+    {
+        private List<Contact> contacts;
+        private Dictionary<string, object> textBoxAttributes = new Dictionary<string, object>
+    {
+         { "disabled","disabled" },
+         { "placeholder","First Name" },
+    };
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(3500);
+            contacts = new List<Contact>
+        {
+            new Contact
+            {
+                firstName = "John",
+                lastName = "Smith",
+                email = "john.smith@example.com"
+            },
+            new Contact
+            {
+                firstName = "Emma",
+                lastName = "Johnson",
+                email = "emma.johnson@example.com"
+            },
+            new Contact
+            {
+                firstName = "Michael",
+                lastName = "Davis",
+                email = "michael.davis@example.com"
+            },
+        };
+            base.OnInitializedAsync();
+        }
+
+    }
+}
