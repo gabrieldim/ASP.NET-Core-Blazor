ed2c15047d26f54383cd6f6abb3af3c7e6cc201b
gabrieldim <dimitrievskig000@gmail.com>
2023-06-16 14:06:56 +0200

    Razor Components and Arbitary Parameters

---
+++ BlazorServerApp/BlazorServerApp/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServerApp.Models
+{
+    [Serializable]
+    public class Contact
+    {
+        public string firstName { get; set; }  
+        public string lastName { get; set; }
+        public string email { get; set; }
+
+    }
+}
