fb91354279a68d87aea470c443bd142e1224bc83
gabrieldim <dimitrievskig000@gmail.com>
2023-06-17 20:17:27 +0200

    added pie about revenue per year

---
+++ BlazorServerApp/BlazorServerApp/Models/Revenue.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServerApp.Models
+{
+    [Serializable]
+    public class Revenue
+    {
+        public float year { get; set; }
+
+        public float amount { get; set; }
+    }
+}
---
+++ BlazorServerApp/BlazorServerApp/Pages/RevenueData.razor.cs
@@ -0,0 +1,51 @@
+﻿using BlazorServerApp.Models;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+
+
+namespace BlazorServerApp.Pages
+{
+    public partial class RevenueData : ComponentBase
+    {
+
+        private List<Revenue> revenueList;
+
+        public RevenueData()
+        {
+            revenueList = new List<Revenue>
+            {
+            new Revenue
+            {
+                year = 2018,
+                amount = 230000,
+            },
+            new Revenue
+            {
+                year = 2019,
+                amount = 280000,
+            },
+            new Revenue
+            {
+                year = 2020,
+                amount = 400000,
+            },
+            new Revenue
+            {
+                year = 2021,
+                amount = 350000,
+            },
+            new Revenue
+            {
+                year = 2022,
+                amount = 580000,
+            },
+            new Revenue
+            {
+                year = 2023,
+                amount = 480000,
+            },
+           };
+        }
+
+    }
+}
