cca2e0078b4c390398fcf262e9db8fcfbda227c2
gabrieldim <dimitrievskig000@gmail.com>
2023-06-17 14:14:58 +0200

    CarList Table

---
+++ BlazorServerApp/BlazorServerApp/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServerApp.Models
+{
+    [Serializable]
+    public class Car
+    {
+        public string name { get; set; }
+        public int year { get; set; }
+        public int price { get; set; }
+        public float km { get; set; }
+    }
+}
